63568241debfe45a5bf4756d3c30238fa7052e18
ww-rm <ww-rm@qq.com>
2021-05-18 20:54:31 +0800

    add example file main.py main.ipynb

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ if __name__ == "__main__":
     spider = WbCmtSpider("_2AkMX_z1ddcPxrAZTnvwQz2ngbYpH-jykKlSrAn7uJhMyAxhu7n8FqSdutBF-XHa3Zsn7ZaBcRYka1AI6UECM-3hy")
     builder = DatasetBuilder("./data/dict/pretrain_wv.index.json", "./data/dict/pretrain_wv.vec.dat", "cuda")
     detector = RumorDetector("cuda").load("./data/model/rmdt.pt")
+    print(detector.model)
 
-    origin, comments = spider.get_comments_by_url("https://weibo.com/1774057271/KfICUisxk")
+    # test data comes from dataset
+    origin, comments = spider.get_comments_by_url("https://m.weibo.cn/detail/11151379535")
+    print(origin)
+    print(len(comments))
 
     raw_input = [origin] + comments
     predict_data = builder.build_input(raw_input)
@@ -15,5 +19,6 @@ if __name__ == "__main__":
     result = detector.predict(predict_data)
     print(result["label"])
     print(result["prob"])
+    print(result["weight"])
 
 
